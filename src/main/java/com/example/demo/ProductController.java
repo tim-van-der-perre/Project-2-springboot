2a55694744ecefb809065320f5d9b91bb090c3b4
Stijn Hendrix <stijn_hendrix@yahoo.com>
2021-04-01 11:11:29 +0200

    partner role

--- src/main/java/com/example/demo/ProductController.java
+++ src/main/java/com/example/demo/ProductController.java
@@ -3,12 +3,23 @@ package com.example.demo;
 import java.util.Arrays;
 import java.util.List;
 
+
+import com.example.demo.Product;
+import com.example.demo.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin( origins = "http://localhost:8080", allowCredentials = "true")
 @RestController
 public class ProductController {
 
@@ -18,30 +29,35 @@ public class ProductController {
     @RequestMapping("/products")
     public List<Product> getAllProducts() {
         return productRepository.findAll();
-        /*
-        return Arrays.asList(
-                new Product(
-                        "jens",
-                        "jens Retina 13.3' ME662 (2013)",
-                        "https://macbookpics.s3.eu-de.cloud-object-storage.appdomain.cloud/img1.jpeg",
-                        5,
-                        2399
-                ),
-                new Product(
-                        "stijn",
-                        "Macbook Pro 13.3' Retina MF841LL/A",
-                        "https://macbookpics.s3.eu-de.cloud-object-storage.appdomain.cloud/img2.jpeg",
-                        3,
-                        1199
-                ),
-                new Product(
-                        "tim",
-                        "Macbook Pro 15.4' Retina MC975LL/A Model 2012",
-                        "https://macbookpics.s3.eu-de.cloud-object-storage.appdomain.cloud/img3.jpeg",
-                        9,
-                        1800
-                )
-        );
-        */
+    }
+
+    @GetMapping("/check")
+    public boolean greeting(@RequestParam(value = "name", defaultValue = "World") String name,
+                            @AuthenticationPrincipal Jwt accessToken) {
+        System.out.println("In GET Request");
+        String scope = accessToken.getClaims().get("scope").toString();
+        Boolean partnerRole = scope.contains("partner");
+        System.out.println("Contains sequence 'partner': " + accessToken.getClaims().get("scope").toString());
+        System.out.println("Contains sequence 'partner': " + accessToken.getClaims().get("scope").toString().contains("partner"));
+        if (partnerRole) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/products")
+    public String addProduct(@RequestBody Product product, @AuthenticationPrincipal Jwt accessToken) {
+        System.out.println("In POST Request");
+        String scope = accessToken.getClaims().get("scope").toString();
+        Boolean partnerRole = scope.contains("partner");
+
+        if (partnerRole) {
+            System.out.println("Contains sequence 'partner': " + accessToken.getClaims().get("scope").toString());
+            System.out.println("Contains sequence 'partner': " + accessToken.getClaims().get("scope").toString().contains("partner"));
+            return "Product added";
+        } else {
+            return "Not Authorized to add product";
+        }
     }
 }
